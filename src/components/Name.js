79e58b6b3e0699bfa47bb1150d8c3202262abb16
anthony62490 <anthonyjmagno@gmail.com>
2018-12-21 16:08:52 -0600

    still trying to import text file

--- src/components/Name.js
+++ src/components/Name.js
@@ -1,22 +1,30 @@
-import React, { Component } from 'react';
-import DWARF_NAMES from '../assets/language_DWARF.txt';
+import React, { Component } from "react";
+var D_NAMES = "test";
 
-class Name extends Component 
-{
-  constructor()
-  {
+class Name extends Component {
+  constructor() {
     super();
     this.state = {
-      name: ""
+      name: "",
+      title: ""
     };
+
+    var file = "../assets/language_DWARF.txt";
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function() {
+      if (rawFile.readyState === 4) {
+        if (rawFile.status === 200 || rawFile.status == 0) {
+          var allText = rawFile.responseText;
+          alert(allText);
+        }
+      }
+    };
+    rawFile.send(null);
   }
-  render() 
-  {
-    return (
-      <div>
-        {DWARF_NAMES}
-      </div>
-    );
+
+  render() {
+    return <div>{D_NAMES}</div>;
   }
 }
 
