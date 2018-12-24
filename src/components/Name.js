39ba5028b135a64f500ebe7a19237f05ac56d7ae
anthony62490 <anthonyjmagno@gmail.com>
2018-12-24 13:00:25 -0600

    prototype random word generation finished

--- src/components/Name.js
+++ src/components/Name.js
@@ -1,30 +1,54 @@
 import React, { Component } from "react";
-var D_NAMES = "test";
+const D_NAMES = require('./assets/language_DWARF.json');
 
-class Name extends Component {
+class Name extends Component 
+{
   constructor() {
     super();
     this.state = {
-      name: "",
-      title: ""
+      original: [],
+      translated: [],
+      rand: null
     };
-
-    var file = "../src/assets/language_DWARF.txt";
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function() {
-      if (rawFile.readyState === 4) {
-        if (rawFile.status === 200 || rawFile.status == 0) {
-          var allText = rawFile.responseText;
-          alert(allText);
-        }
-      }
-    };
-    rawFile.send(null);
   }
 
-  render() {
-    return <div>{D_NAMES}</div>;
+  setRand()
+  {
+    this.setState({rand: Math.floor(Math.random() * this.state.original.length)})
+  }
+
+  /*
+  Language blocks should appear in this format
+  {
+    DWARVEN: {
+
+    }
+  }
+  */
+
+  componentDidMount()
+  {
+    let rippedKeys = [];
+    let rippedValues = [];
+    for(let i = 0; i<D_NAMES.length; i++)
+    {
+      rippedKeys.push(D_NAMES[i]["original"]);
+      rippedValues.push(D_NAMES[i]["translated"]);
+    }
+    this.setState({
+      original: rippedKeys, 
+      translated: rippedValues
+    });
+    this.setRand();
+  }
+
+  render() 
+  {
+    let { rand } = this.state;
+    return <div>
+      <p className="name">{this.state.original[rand]} -> {this.state.translated[rand]}</p>
+      <button onClick={() => this.setRand()}>rand = {rand}</button>
+    </div>;
   }
 }
 
