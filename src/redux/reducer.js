411c2cd2b5a8311fd36958d28ff5cdb66f55a541
anthony62490 <anthonyjmagno@gmail.com>
2018-12-28 22:35:49 -0600

    prepping to move language files to redux

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,4 +1,9 @@
 import axios from 'axios';
+const DWARF = require('./assets/language_DWARF.json');
+const ELF = require('./assets/language_ELF.json');
+const HUMAN = require('./assets/language_HUMAN.json');
+const GOBLIN = require('./assets/language_GOBLIN.json');
+const WORD_TYPES = require('./assets/language_words.json');
 
 //CONSTANTS
 const INITIALIZE_LANGUAGES = 'INITIALIZE_LANGUAGES';
@@ -7,7 +12,7 @@ const MERGE_LANGUAGES = 'MERGE_LANGUAGES';
 // INITIAL APP STATE
 const initialState = 
 {
-  langInit = 0,
+  langInit = false,
   english = [],
   dwarf = [],
   elf = [],
@@ -32,6 +37,14 @@ export default function generalReducer(state = initialState, action)
 }
 
 // ACTION CREATORS
+
+export function initializeLanguages()
+{
+  return {
+    type: INITIALIZE_LANGUAGES,
+    payload: ""
+  };
+}
 
 export function mergeLanguages() 
 {
