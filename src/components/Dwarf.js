31ef33adb25b0d6570598492576badd00ab361e7
anthony62490 <anthonyjmagno@gmail.com>
2019-01-02 12:55:35 -0600

    added race-specific naming preferences

--- src/components/Dwarf.js
+++ src/components/Dwarf.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 import { initializeLanguages } from '../redux/reducer';
+import './Dwarf.css';
 
 class Dwarf extends Component 
 {
@@ -11,9 +12,14 @@ class Dwarf extends Component
     {
       namesThisSession: 0,
       currentDwarf: {
-          firstName: "",
-          lastName: ""
-      }
+        firstName: "",
+        lastName: ""
+      },
+      allNameTokens: ["flowery", "nature", "primitive", "holy", "evil", "negator", "magic", "violent", "peace", "ugly", "death", "old", "subordinate", "leader", "new", "domestic", "mythic", "artifice", "color", "mystery", "negative", "romantic", "assertive", "aquatic", "protect", "restrain", "thought", "wild", "earth", "good", "balance", "boundary", "dance", "darkness", "light", "order", "festival", "family", "fire", "food", "freedom", "games", "luck", "music", "sky", "silence", "trade", "travel", "truth", "wealth"],
+      dwarfNameTokens: [["artifice", "earth"], ["domestic", "subordinate", "evil", "flowery", "negative", "ugly", "negator"]],
+      elfNameTokens: [["flowery", "nature"], ["domestic", "subordinate", "evil", "negative", "ugly", "negator"]],
+      humanNameTokens: [[], ["subordinate", "evil", "negative", "ugly", "negator"]],
+      goblinNameTokens: [["evil"], ["domestic", "flowery", "holy", "peace", "negator", "good"]]
     };
   }
 
@@ -33,17 +39,17 @@ class Dwarf extends Component
     let word = this.props.english[num];
     if(word)
     {
-        // console.log("english word", word);
-        // console.log("num", num);
-        // console.log("type", type);
-        let typeArray = this.props.grammar[word];
-        return (typeArray.includes(type));
+      // console.log("english word", word);
+      // console.log("num", num);
+      // console.log("type", type);
+      let typeArray = this.props.grammar[word];
+      return (typeArray.includes(type));
     }
     else return true ;
   }
 
   capitalize(string) {
-      return string.charAt(0).toUpperCase() + string.slice(1);
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
   getDwarfName()
@@ -51,31 +57,32 @@ class Dwarf extends Component
     let first;
     do 
     {
-        first = this.getRand();
+      first = this.getRand();
     } while(!this.wordIsOfType(first, "noun"));
     let last1 = this.getRand();
     let last2 = this.getRand();
 
     let dwarfName = {
-        firstName: this.capitalize(this.props.dwarf[first]),
-        lastName: this.capitalize(this.props.dwarf[last1] + this.props.dwarf[last2]),
-        transLastName: this.capitalize(this.props.english[last1] + this.props.english[last2])
+      firstName: this.capitalize(this.props.dwarf[first]),
+      lastName: this.capitalize(this.props.dwarf[last1] + this.props.dwarf[last2]),
+      transLastName: this.capitalize(this.props.english[last1] + this.props.english[last2])
     };
     this.setState({ currentDwarf: dwarfName, namesThisSession: this.state.namesThisSession+1 })
   }
 
   render() 
   {
-      let nameBlock = <div>
-        {this.state.currentDwarf.firstName} "{this.state.currentDwarf.transLastName}" {this.state.currentDwarf.lastName}<br/>
-
-      </div>;
+    let nameBlock = <div className="dwarf-name">
+      {this.state.currentDwarf.firstName} "{this.state.currentDwarf.transLastName}" {this.state.currentDwarf.lastName}<br/>
+    </div>;
 
     return <div>
+      <div className="dwarf-module">
         {this.state.namesThisSession > 0 ?
-            nameBlock
-            : null}
+          nameBlock
+          : null}
         <button onClick={() => this.getDwarfName()}>Get D0rf Name</button>
+      </div>
     </div>;
   }
 }
