655219a0f2a47157d47c580aa44719de02d47782
anthony62490 <anthonyjmagno@gmail.com>
2019-01-16 19:22:54 -0600

    created simple trinary toggle component

---
+++ src/components/ThreeState.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './ThreeState.scss';
+
+class ThreeState extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value: 0
+    };
+  }
+  render() {
+    return (
+      <div>
+        <input 
+          className="trinary-toggle"
+          style={{background: "red"}}
+          type="range"
+          value={this.state.value}
+          onChange={(e) => this.setState({value: e.target.value})}
+          min="-1"
+          max="1"
+          step="1"
+        />
+      </div>
+    );
+  }
+}
+
+export default ThreeState;
