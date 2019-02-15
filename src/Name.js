e8cbe314eab9c59c4ce39c28361d81b2926329c4
anthony62490 <anthonyjmagno@gmail.com>
2019-02-14 23:28:11 -0600

    planning stages for refactoring

--- src/Name.js
+++ src/Name.js
@@ -2,3 +2,7 @@
 // Contains the generic getName function for getting the name of a civ entity. 
 // Not suitable for FBs or artifacts except in special cases.
 //
+
+// getName() //Should take in a language and a word pool. Outputs a standard name in the given language using the given pool
+
+// wordIsOfType() //Grammar chaecking function. Checks to see that a given word has a given form
