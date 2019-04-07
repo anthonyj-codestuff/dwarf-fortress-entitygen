7c160e7ec16d5d6786c3f3cbe8485c3034c0d322
anthony62490 <anthonyjmagno@gmail.com>
2019-04-07 03:28:35 -0500

    import languages and implement something simple

--- src/components/NameModule.js
+++ src/components/NameModule.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import * as Constants from "./assets/languages";
 
 class NameModule extends Component {
+  constructor(){
+    super();
+    this.state = {
+      dwarfName: ""
+    };
+  }
+
+  componentDidMount(){
+    this.setState({dwarfName: Constants.dwarf[0]});
+  }
+
   render() {
     return (
       <div>
-        TEST
+        {this.state.dwarfName}
       </div>
     );
   }
