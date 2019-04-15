8b6b46af74f18b295949d1da84d6797b912819d8
anthony62490 <anthonyjmagno@gmail.com>
2019-04-14 23:28:10 -0500

    added documentation

--- src/components/partials/getName.js
+++ src/components/partials/getName.js
@@ -1,8 +1,14 @@
+/** Constants: An object full of arrays. Each array's key describes its theme */
 import * as Constants from "../assets/languages";
 
+/**
+ * Calls getPool() for an array of valid names
+ * @param {*} tags [[required tags],[forbidden tags]]
+ * @param {*} language A string from the list of defined languages
+ * @returns An object with the requested name in three pieces
+ */
 export function getName(tags, language) {
   let fullPool = getPool(tags[0], language);
-  console.log(Constants.tokens.length);
   this.setState({entityName: {...this.state.entityName, first: language}});
 }
 
@@ -10,7 +16,7 @@ function getPool(posTags, language) {
   let pool = [];
   if (posTags.length > 0) {
     posTags.forEach(e => {
-      console.log("looping through posTags", e);
+      console.log("posTags loop", e);
       Constants.tokens[e].forEach(f => pool.push(f))
     });
   } else {
