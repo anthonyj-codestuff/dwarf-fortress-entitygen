d7046a85379978a3c47e94d4f95eb94b4d65b080
anthony62490 <anthonyjmagno@gmail.com>
2019-04-14 23:40:10 -0500

    trimmed down pool code

--- src/components/partials/getName.js
+++ src/components/partials/getName.js
@@ -8,18 +8,21 @@ import * as Constants from "../assets/languages";
  * @returns An object with the requested name in three pieces
  */
 export function getName(tags, language) {
-  let fullPool = getPool(tags[0], language);
+  let fullPool = getPool(tags[0]);
   this.setState({entityName: {...this.state.entityName, first: language}});
 }
 
-function getPool(posTags, language) {
+function getPool(posTags) {
   let pool = [];
   if (posTags.length > 0) {
     posTags.forEach(e => {
       console.log("posTags loop", e);
-      Constants.tokens[e].forEach(f => pool.push(f))
+      Constants.tokens[e].forEach(f => pool.push(f));
     });
   } else {
 
   }
+
+  // remove dupes
+  pool = pool.filter((e,i,s) => (e !== "" && s.indexOf(e) === i));
 }
